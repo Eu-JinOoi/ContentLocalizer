58aee7f89b25386862b9e884170aa0ee5a1573c3
Eu-Jin Ooi <Ooi@Eu-J.in>
2019-07-14 00:16:18 -0700

    Added basic hashing to determine if a file has already been seen"

--- test.py
+++ test.py
@@ -3,29 +3,72 @@ import sys
 import time
 import logging
 import os
+import hashlib
+import shutil
 from pprint import pprint
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
 from watchdog.events import PatternMatchingEventHandler
 
-def get_files_in_directory(path, files_to_copy, fileExtensionFilter=None):
+class FileHashes:
+    HashTable = dict() 
+    def __init__(self):
+        #TODO Load Hash Table
+        print("Need to load HashTable");
+        self.HashTable = dict()
+    def file_seen(self, absolute_path):
+        hashValue = self.get_file_hash(absolute_path)
+        if hashValue in self.HashTable:
+            return True
+        return False
+    def add_file(self, absolute_path):
+        hashValue = self.get_file_hash(absolute_path)
+        self.HashTable[hashValue]=True
+
+    def get_file_hash(self, absolute_path):
+        BLOCKSIZE = 65536
+        hasher = hashlib.sha1()
+        with open(absolute_path, 'rb') as afile:
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        return hasher.hexdigest()
+    def __del__(self):
+        #TODO Save Hash Table
+        print "Destuct"
+
+HashHandler = FileHashes(); #TODO [OOI] Find a better way to inject this
+def get_files_in_directory(path, fileExtensionFilter=None):
+    files_to_copy=[]
     try:
         for fileInPath in os.listdir(path):
             fullPath = os.path.join(path,fileInPath);
             if os.path.isdir(fullPath) is True:
-                print("Is Directory: "+ fullPath)
-                get_files_in_directory(fullPath, fileExtensionFilter)
+                print("Directory: "+ fullPath)
+                subdirectory_files_to_copy=get_files_in_directory(fullPath, fileExtensionFilter)
+                files_to_copy = files_to_copy + subdirectory_files_to_copy
             elif os.path.islink(fullPath) is True:
-                print("Is Link: "+ fullPath)
+                print("Link: "+ fullPath)
             elif os.path.isfile(fullPath) is True and (fileExtensionFilter is None or fileInPath.endswith(tuple(fileExtensionFilter))):
                 files_to_copy.append(fullPath)
-                print(" File:"+ fullPath)
+                print("  File:"+ fullPath)
     except Exception as e:
         print "Directory Unavailable:"+str(e)
+    return files_to_copy
+def copyFiles(fileHashes,  files_to_copy):
+    print("==============COPY=============")
+    for file_to_copy in files_to_copy:
+        if fileHashes.file_seen(file_to_copy) is False:
+            print ("Need to Copy: "+file_to_copy)
+            fileHashes.add_file(file_to_copy)
+        else:
+            print ("Already seen: "+file_to_copy);
+
 def on_created(event):
     allowed_extensions = ["jpg","jpeg","JPG","JPEG","tiff","TIFF"]
     if(os.path.ismount(event.src_path)):
-        get_files_in_directory(event.src_path, allowed_extensions)
+        files_to_copy = get_files_in_directory(event.src_path, allowed_extensions)
+        copyFiles(HashHandler, files_to_copy);
     else:
         print("Not Mount Point: " + event.src_path)
 
@@ -40,7 +83,6 @@ def on_moved(event):
     print("Moved: "+event.src_path)
     #pprint(event)
 if __name__ == "__main__":
-    #patterns=["*.jpg","*.jpeg","*.JPG","*.JPEG","*.png","*.tiff"]
     patterns="*"
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
